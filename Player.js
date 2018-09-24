210ee0b194b783332bbdd6b3bba858e192e88c78
Tibi <bukkfabiszti@gmail.com>
2018-09-24 10:59:39 +0200

    get player func

--- Player.js
+++ Player.js
@@ -16,6 +16,9 @@ class Player {
 module.exports = Player;
 
 let player;
+let cards;
+let card1;
+let card2;
 
 function getPlayer(game) {
   for (let p in game) {
@@ -23,4 +26,24 @@ function getPlayer(game) {
       player = p;
     }
   }
+  getCards(player);
 }
+
+function getCards(player) {
+  cards = player.hole_cards;
+  card1 = cards[0];
+  card2= cards[1];
+}
+
+function shouldIBet(cards) {
+  switch (card1.rank) {
+    case card2.rank: return true;
+    case "A": return true;
+    case "Q": return true;
+    case "K": return true;
+  }
+  return false;
+}
+
+
+
