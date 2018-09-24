6a24dabc655863beaf28ec46f6f277892da74566
kinga00099 <kinga00099@gmail.com>
2018-09-24 10:46:15 +0200

    get player hole cards

--- Player.js
+++ Player.js
@@ -6,6 +6,12 @@ class Player {
   static betRequest(gameState, bet) {
     bet(100);
     let Game = gameState.parse();
+    let holeCards;
+    for (let player of Game.players){
+      if (player.name === "Legnehezebb"){
+        holeCards = player.hole_cards;
+      }
+    }
   }
 
   static showdown(gameState) {
