a12cfa1565b6dbbbdb254f986e401fd5057d3b14
Tibi <bukkfabiszti@gmail.com>
2018-09-24 11:21:08 +0200

    card checking functions

--- Player.js
+++ Player.js
@@ -23,7 +23,7 @@ module.exports = Player;
 
 let player;
 let cards;
-let card1;
+let card2;
 let card2;
 
 function getPlayer(game) {
@@ -37,11 +37,11 @@ function getPlayer(game) {
 
 function getCards(player) {
   cards = player.hole_cards;
-  card1 = cards[0];
+  card2 = cards[0];
   card2= cards[1];
 }
 
-function shouldIBet(cards) {
+function checkFirstCard(card1) {
   switch (card1.rank) {
     case card2.rank: return true;
     case "A": return true;
@@ -50,6 +50,25 @@ function shouldIBet(cards) {
   }
   return false;
 }
+
+function checkSecondCard(card2) {
+  switch (card2.rank) {
+    case card1.rank: return true;
+    case "A": return true;
+    case "Q": return true;
+    case "K": return true;
+  }
+  return false;
+}
+
+//returns true if one of the two cards are good, or we have a pair
+function checekBothCards(card1, card2) {
+  if (checkFirstCard(card1) || checkSecondCard(card2)) {
+    return true;
+  }
+  return false;
+}
+
 
 
 
